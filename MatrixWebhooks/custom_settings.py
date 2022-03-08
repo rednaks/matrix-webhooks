87a55af0e9ba90626c68f904f864beb050392bf3
SkanderBM <skander.bmahmoud@gmail.com>
2022-03-08 17:34:20 +0100

    MatrixWebhooks app

---
+++ MatrixWebhooks/custom_settings.py
@@ -0,0 +1,5 @@
+CONSTANCE_CONFIG = {
+    'MATRIX_HOMESERVER': ('', 'Matrix Home Server url (or e2e proxy url)', str),
+    'MATRIX_BOT_USERNAME': ('', 'Bot username, eg: @webhook:matrix-webhooks.com', str),
+    'MATRIX_BOT_TOKEN': ('', 'Bot token', str),
+}
