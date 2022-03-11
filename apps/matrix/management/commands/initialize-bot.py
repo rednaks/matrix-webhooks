7a7ad1066fd4cb1d072c61b965964671d36dcb97
SkanderBM <skander.bmahmoud@gmail.com>
2022-03-11 10:46:40 +0100

    add optional homeserver arg

--- apps/matrix/management/commands/initialize-bot.py
+++ apps/matrix/management/commands/initialize-bot.py
@@ -8,9 +8,11 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         matrix_config = get_matrix_config()
         matrix_config.password = options.get('password')
+        matrix_config.homeserver = options.get('homeserver') or config.MATRIX_HOMSERVER
         response = bot.login(matrix_config)
         self.stdout.write(f"New token: {response.access_token}")
         setattr(config, "MATRIX_BOT_TOKEN", response.access_token)
 
     def add_arguments(self, parser):
         parser.add_argument('--password', dest='password', type=str)
+        parser.add_argument('--homeserver', dest='homeserver', type=str, required=False)
