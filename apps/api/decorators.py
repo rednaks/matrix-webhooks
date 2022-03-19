b6849d3dc4945b5c10a4accf3f6b240c771d871e
SkanderBM <skander.bmahmoud@gmail.com>
2022-03-19 17:00:22 +0100

    rate limit redis

--- apps/api/decorators.py
+++ apps/api/decorators.py
@@ -1,5 +1,8 @@
 from functools import wraps
 
+from apps.helpers import get_redis_client
+from constance import config
+
 
 class RateLimitException(Exception):
     ...
@@ -9,8 +12,15 @@ def ratelimit(func):
     @wraps(func)
     def wrapper(request, *a, **kw):
         print(f"token : {kw['user_token']}")
-        if False:
+        token = kw['user_token']
+        redis_client = get_redis_client()
+        key = f"ratelimit:{token}"
+
+        if int(redis_client.incr(key)) > config.API_RATE_LIMIT:
             raise RateLimitException("Too Many Requests")
+
+        if redis_client.ttl(key) == -1:
+            redis_client.expire(key, 1)
 
         return func(request, *a, **kw)
 
