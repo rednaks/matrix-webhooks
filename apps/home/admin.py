0f7370592b4f9a6fa56620bdd48815727147d6a4
SkanderBM <skander.bmahmoud@gmail.com>
2022-03-24 08:32:07 +0100

    add waiting list to admins - #6

--- apps/home/admin.py
+++ apps/home/admin.py
@@ -1,9 +1,14 @@
 from django.contrib import admin
 
-from .models import UserAccountModel
+from .models import UserAccountModel, WaitingListUserAccountModel
 
 
 # Register your models here.
 @admin.register(UserAccountModel)
 class UserAccountModelAdmin(admin.ModelAdmin):
     list_display = ('token', 'user')
+
+
+@admin.register(WaitingListUserAccountModel)
+class UserAccountModelAdmin(admin.ModelAdmin):
+    list_display = ('user',)
