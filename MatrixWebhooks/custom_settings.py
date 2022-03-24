c31aed92ac068233a708dfca8205c5a881333c17
SkanderBM <skander.bmahmoud@gmail.com>
2022-03-24 08:24:27 +0100

    organize settings in sections - #6

--- MatrixWebhooks/custom_settings.py
+++ MatrixWebhooks/custom_settings.py
@@ -1,3 +1,5 @@
+from collections import OrderedDict
+
 CONSTANCE_CONFIG = {
     'MATRIX_HOMESERVER': ('', 'Matrix Home Server url (or e2e proxy url)', str),
     'MATRIX_BOT_USERNAME': ('', 'Bot username, eg: @webhook:matrix-webhooks.com', str),
@@ -7,3 +9,12 @@ CONSTANCE_CONFIG = {
     'INVITATION_CODE': ("", 'Code Signup', str),
     'INVITATIONS': (0, 'Number of invitations', int),
 }
+
+CONSTANCE_CONFIG_FIELDSETS = OrderedDict({
+    'Bot Settings': {
+        'fields': ('MATRIX_HOMESERVER', 'MATRIX_BOT_USERNAME', 'MATRIX_BOT_TOKEN'),
+    },
+    'Site Settings': {
+        'fields': ('API_RATE_LIMIT', 'INVITATION_ONLY', 'INVITATION_CODE', 'INVITATIONS'),
+    }
+})
