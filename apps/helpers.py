664241c974b14a580a1dea1302ed85df9e51d7f3
SkanderBM <skander.bmahmoud@gmail.com>
2022-03-26 18:38:02 +0100

    moving invitation key helpers to helpers.py

--- apps/helpers.py
+++ apps/helpers.py
@@ -13,3 +13,17 @@ def get_redis_client() -> redis.Redis:
 
 def make_invitation_code_key(invitation_code):
     return f"INVITATION_CODE:{invitation_code.lower()}"
+
+
+PENDING_INVITATIONS_PREFIX = "pending_invitations"
+INCIDENTS_CACHE_KEY_PREFIX = "invitation_incident"
+
+
+def get_pending_invitation_key(key: str) -> str:
+    return f"{PENDING_INVITATIONS_PREFIX}:{key}"
+
+
+def get_invitation_incident_key(user_id: str, room_id: str) -> str:
+    return f"{INCIDENTS_CACHE_KEY_PREFIX}:{user_id}:{room_id}"
+
+
