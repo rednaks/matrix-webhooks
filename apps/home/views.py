9a7404d76044634f030c1f8d4efbe351aa9d5b6b
SkanderBM <skander.bmahmoud@gmail.com>
2023-03-07 07:10:01 +0100

    formatting + add captcha verification - matrix-webooks#19

--- apps/home/views.py
+++ apps/home/views.py
@@ -1,25 +1,26 @@
-from django.shortcuts import render, redirect
-from django.urls import reverse
-from django.contrib.auth import get_user_model, logout
-from django.contrib import messages
-from django.core.mail import send_mail
-from constance import config
-
+import logging
 import threading
 
+import requests
+from constance import config
+from django.contrib import messages
+from django.contrib.auth import get_user_model, logout
+from django.core.mail import send_mail
+from django.shortcuts import redirect, render
+from django.urls import reverse
 from sesame.utils import get_query_string, get_user
 
-from apps.home.forms import LoginForm, AuthForm
-from .models import UserAccountModel, WaitingListUserAccountModel
-
 from apps.handlers import AvailableSources
+from apps.home.forms import AuthForm, LoginForm
+
 from ..helpers import get_redis_client, make_invitation_code_key
+from .models import UserAccountModel, WaitingListUserAccountModel
 
 
 def home(request):
     ctx = {
-        'login_url': reverse('login_view'),
-        'invitation_only': config.INVITATION_ONLY
+        "login_url": reverse("login_view"),
+        "invitation_only": config.INVITATION_ONLY,
     }
 
     if request.user.is_authenticated:
@@ -29,26 +30,57 @@ def home(request):
         except UserAccountModel.DoesNotExist:
             token = _generate_token_for_user(request.user)
 
-        ctx['token'] = token
-        ctx['notify_urls'] = []
+        ctx["token"] = token
+        ctx["notify_urls"] = []
 
         for source in AvailableSources:
             kwargs = {
-                'user_token': token,
-                'room_id': 'your_matrix_room_id',
+                "user_token": token,
+                "room_id": "your_matrix_room_id",
             }
 
             if source.value:
-                kwargs['source'] = source.value
+                kwargs["source"] = source.value
 
-            reverse_notify = reverse('api-1:notify', kwargs=kwargs)
+            reverse_notify = reverse("api-1:notify", kwargs=kwargs)
 
-            ctx['notify_urls'].append({
-                'url': request.build_absolute_uri(reverse_notify),
-                'type': source.value or 'custom'
-            })
+            ctx["notify_urls"].append(
+                {
+                    "url": request.build_absolute_uri(reverse_notify),
+                    "type": source.value or "custom",
+                }
+            )
 
-    return render(request, 'home/index.html', context=ctx)
+    return render(request, "home/index.html", context=ctx)
+
+
+def _verify_captcha(solution: str) -> bool:
+    if not config.FRIENDLY_CAPTCHA_ENABLED:
+        return True
+
+    verification = requests.post(
+        "https://api.friendlycaptcha.com/api/v1/siteverify",
+        json={
+            "solution": solution,
+            "secret": config.FRIENDLY_CAPTCHA_KEY,
+            "sitekey": config.FRIENDLY_CAPTCHA_SITE_KEY,
+        },
+    )
+
+    if verification.ok:
+        try:
+            response = verification.json()
+            if response.get("success", False):
+                return True
+            else:
+                logging.info(f"Unable to verify captcha: {response}")
+                return False
+        except Exception:
+
+            return False
+    else:
+        logging.info(f"Unable to verify captcha, api error: {verification.text}")
+        return False
 
 
 def login_view(request):
@@ -56,10 +88,20 @@ def login_view(request):
         login_form = LoginForm(request.POST)
 
         if login_form.is_valid():
-            email = login_form.cleaned_data['email'].lower()
-            invitation_code = login_form.cleaned_data['invitation_code']
+            email = login_form.cleaned_data["email"].lower()
+            invitation_code = login_form.cleaned_data["invitation_code"]
+            captcha_solution = login_form.cleaned_data["captcha_solution"]
+            # verify
+            if not _verify_captcha(captcha_solution):
+                messages.add_message(
+                    request, messages.ERROR, "Couldn't verify you're a human."
+                )
+                return redirect("home")
+
             user_model = get_user_model()
-            requested_user, created = user_model.objects.get_or_create(username=email, email=email)
+            requested_user, created = user_model.objects.get_or_create(
+                username=email, email=email
+            )
 
             # Generate token for account if created
             try:
@@ -69,38 +111,48 @@ def login_view(request):
 
             try:
                 WaitingListUserAccountModel.objects.get(user=requested_user)
-                messages.add_message(request, messages.INFO,
-                                     f"You're in the waiting list, you will be notified when your account is enabled.")
+                messages.add_message(
+                    request,
+                    messages.INFO,
+                    f"You're in the waiting list, you will be notified when your account is enabled.",
+                )
             except WaitingListUserAccountModel.DoesNotExist:
                 # user exist and not in waiting list. continue login process
                 token_qs = get_query_string(requested_user)
 
-                magic_link = request.build_absolute_uri(reverse('login_view'))
+                magic_link = request.build_absolute_uri(reverse("login_view"))
 
-                magic_link = f'{magic_link}{token_qs}'
+                magic_link = f"{magic_link}{token_qs}"
 
                 # TODO: celery
                 async_task(send_magic_link, requested_user, magic_link)
 
-                messages.add_message(request,
-                                     messages.SUCCESS,
-                                     f'A link was sent to {email}. Please check your inbox to login.'
-                                     )
+                messages.add_message(
+                    request,
+                    messages.SUCCESS,
+                    f"A link was sent to {email}. Please check your inbox to login.",
+                )
+        else:
+            messages.add_message(
+                request, messages.ERROR, "Couldn't verify you're a human."
+            )
 
     elif request.GET:
         auth_form = AuthForm(request.GET)
         if auth_form.is_valid():
             user = get_user(request)
             if user is None:
-                messages.add_message(request, messages.ERROR, 'Link expired, please login again')
+                messages.add_message(
+                    request, messages.ERROR, "Link expired, please login again"
+                )
 
-    return redirect('home')
+    return redirect("home")
 
 
 def logout_view(request):
     logout(request)
-    messages.add_message(request, messages.INFO, 'Logged out.')
-    return redirect('home')
+    messages.add_message(request, messages.INFO, "Logged out.")
+    return redirect("home")
 
 
 def send_magic_link(user, link):
@@ -108,7 +160,7 @@ def send_magic_link(user, link):
     Use this magic link to login to Matrix-Webhooks: <a clicktracking=off href="{link}">{link}</a>
     """
     send_mail(
-        subject='[Matrix-Webhooks] Login magic link',
+        subject="[Matrix-Webhooks] Login magic link",
         message=mail_content,
         html_message=mail_content,
         from_email=None,
@@ -145,4 +197,6 @@ def _create_user_account(invitation_code, requested_user) -> None:
 
 
 def _add_to_waiting_list(user):
-    waiting_list_account, _ = WaitingListUserAccountModel.objects.get_or_create(user=user)
+    waiting_list_account, _ = WaitingListUserAccountModel.objects.get_or_create(
+        user=user
+    )
