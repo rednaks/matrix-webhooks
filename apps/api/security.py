c269826b72be359d1c03c05d441ae83ddd8bef1f
SkanderBM <skander.bmahmoud@gmail.com>
2023-05-28 21:37:42 +0200

    formatting - #22

--- apps/api/security.py
+++ apps/api/security.py
@@ -1,21 +1,22 @@
 import logging
-from typing import Optional, Any
+from typing import Any, Optional
 
 from django.conf import settings
 from django.http import HttpRequest
 from django.urls import resolve
-
 from ninja.security.apikey import APIKeyBase
+
 from apps.home.models import UserAccountModel
 
 logger = logging.getLogger(settings.LOGGER_NAME)
 
+
 class APIKeyPath(APIKeyBase):
-    openapi_type: str = 'apikey'
-    openapi_in: str = 'user_token'
+    openapi_type: str = "apikey"
+    openapi_in: str = "user_token"
 
     def _get_key(self, request: HttpRequest) -> Optional[str]:
-        key = resolve(request.path).kwargs.get('user_token')
+        key = resolve(request.path).kwargs.get("user_token")
         return key
 
     def authenticate(self, request: HttpRequest, key) -> Optional[Any]:
